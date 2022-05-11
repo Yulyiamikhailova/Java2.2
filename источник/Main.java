c0e9f0379eeadb7d0d698f562bf435a12f017fea
Yulia <98048263+Yulyiamikhailova@users.noreply.github.com>
2022-05-11 12:51:06 +0300

    Update and rename src/Main.java to источник/Main.java

--- источник/Main.java
+++ источник/Main.java
@@ -2,24 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Мобильный кошелек");
 
-        System.out.println("Пополнение от 07.05.2022 г.");
-
-        int amount1 = 100;
-        int add1 = 100;
-        int total = amount1 + add1;
-        int bonus1 = add1 / 100;
-        if (add1 > 1000) {
-            add1 = 1000;
-        } else {
-            bonus1 = 0;
-        }
-
-        System.out.println("Бонуc: " + bonus1);
-        System.out.println("Итоговый счет: " + total + " рубль(ей)");
-
-
-        System.out.println("Пополнение от 09.05.2022 г.");
-
         int amount2 = 100;
         int add2 = 1100;
         int total2 = amount2 + add2;
